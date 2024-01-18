e9281c48854bc4caf32f2b28dc29af38f7346fef
Пользователь <jumanazarovdiyorbek270@gmail.com>
2024-01-18 08:24:16 +0500

    uzum

--- src/context/wishesSlice.js
+++ src/context/wishesSlice.js
@@ -3,14 +3,16 @@ import { createSlice } from "@reduxjs/toolkit";
 export const wishesSlice = createSlice({
     name: "wishes",
     initialState: {
-        value: []
+        value: JSON.parse(localStorage.getItem("wishes")) || []
     },
     reducers: {
         addToWishes(state, action){
             state.value = [...state.value, action.payload]
+            localStorage.setItem("wishes", JSON.stringify(state.value))
         },
         removeFromWishes(state, action){
             state.value = state.value.filter(el => el.id !== action.payload.id)
+            localStorage.setItem("wishes", JSON.stringify(state.value))
         }
     }  
 })
